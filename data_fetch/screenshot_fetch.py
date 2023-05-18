fb42af50b51e41333d270f698262e3393fc9e46e
Krzysztof Baran <krysbaran@gmail.com>
2023-05-18 23:18:20 +0200

    Screen shot taking script

--- data_fetch/screenshot_fetch.py
+++ data_fetch/screenshot_fetch.py
@@ -2,6 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import random
 
 
@@ -12,9 +13,13 @@ class Browser:
     def __init__(self, driver: str):
         self.service = Service(driver)
         self.browser = webdriver.Chrome(service=self.service)
+        self.count = 1
 
     def open_page(self, url: str):
         self.browser.get(url)
+    
+    def sleep(self, start_range: int, end_range: int):
+        time.sleep(random.randint(start_range, end_range))
 
     def close_browser(self):
         self.browser.close()
@@ -22,20 +27,29 @@ class Browser:
     def add_input(self, by: By, value: str, text: str):
         field = self.browser.find_element(by=by, value=value)
         field.send_keys(text)
-        time.sleep(1)
+        self.sleep(1, 4)
 
     def click_button(self, by: By, value: str):
         button = self.browser.find_element(by=by, value=value)
         button.click()
-        time.sleep(1)
+        self.sleep(1, 4)
+
+    def click_execute(self, by: By, value):
+        button = self.browser.find_element(by=by, value=value)
+        self.browser.execute_script("arguments[0].click();", button)
+        
 
     def login_collegerama(self, username: str, password: str):
         self.add_input(by=By.ID, value='username', text=username)
-        time.sleep(random.randint(1, 3))
         self.add_input(by=By.ID, value='password', text=password)
-        time.sleep(random.randint(1, 3))
         self.click_button(by=By.CLASS_NAME, value='login-button')
-
+    
+    def iframe_switch(self, value: str):
+        self.browser.switch_to.frame(value)
+    
+    def screen_shot(self, name: str):
+        self.browser.save_screenshot(f"screenshots/{name}_{self.count}.png")
+        self.count += 1
 
 if __name__ == '__main__':
     
@@ -44,11 +58,42 @@ if __name__ == '__main__':
     
     browser = Browser('/usr/bin/chromedriver')
 
-    browser.open_page('https://collegeramacolleges.tudelft.nl/new/portfolio-items/eemcs-msc-cs/')
-    time.sleep(random.randint(4, 10))
+    browser.open_page('https://collegerama.tudelft.nl/Mediasite/Channel/eemcs-msc-cs/watch/770911dbcaad427990eceb9afe0e01db1d')
+    browser.sleep(1, 3)
     
 
     browser.login_collegerama(username=username, password=password)
-    time.sleep(10)
+    browser.sleep(5, 7)
+    
+    browser.click_button(By.XPATH, value='//*[@id="InAppPlayerContainer"]/div[2]/div[1]')
+    browser.sleep(1, 3)
+
+
+    # ENTER the media player
+    browser.iframe_switch("player-iframe")
+    browser.sleep(1, 3)
+    
+    
+    browser.click_execute(By.XPATH, value='//*[@id="vjs_video_3"]/div[4]/button[1]')
+    browser.sleep(1, 3)
+    
+    browser.click_button(By.XPATH, value='//*[@id="vjs_video_3"]/div[7]/div[10]/div[1]/div[2]')
+    browser.sleep(1, 3)
+    
+    browser.screen_shot("slideshow")
+    browser.sleep(1, 3)
+    
+    browser.click_button(By.CLASS_NAME, value="mediasite-player__smart-zoom-exit-button")
+    browser.sleep(1, 3)
+    
+    browser.click_button(By.XPATH, value='//*[@id="vjs_video_3"]/div[7]/div[10]/div[1]/div[3]')
+    browser.sleep(1, 3)
+    
+    browser.screen_shot("presenter")
+    browser.sleep(1, 3)
+    
+    browser.click_button(By.CLASS_NAME, value="mediasite-player__smart-zoom-exit-button")
+    browser.sleep(1, 3)
 
     browser.close_browser()
+
