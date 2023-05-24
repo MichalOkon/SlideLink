03e7b865e59a7151e87b22c1098ff56a81e5b5a7
MichalOkon <michalionis123@gmail.com>
2023-05-24 14:58:46 +0200

    Add basic screenshot functionality

--- data_fetch/screenshot_fetch.py
+++ data_fetch/screenshot_fetch.py
@@ -7,36 +7,36 @@ import random
 
 
 class Browser:
-    browser, service = None, None
 
     # Initialise the webdriver with the path to chromedriver.exe
     def __init__(self, driver: str):
         self.service = Service(driver)
-        self.browser = webdriver.Chrome(service=self.service)
-        self.count = 1
+        self.driver = webdriver.Chrome(service=self.service)
+        self.slideshow_count = 1
+        self.presenter_count = 1
 
     def open_page(self, url: str):
-        self.browser.get(url)
+        self.driver.get(url)
     
     def sleep(self, start_range: int, end_range: int):
         time.sleep(random.randint(start_range, end_range))
 
     def close_browser(self):
-        self.browser.close()
+        self.driver.close()
 
     def add_input(self, by: By, value: str, text: str):
-        field = self.browser.find_element(by=by, value=value)
+        field = self.driver.find_element(by=by, value=value)
         field.send_keys(text)
         self.sleep(1, 4)
 
     def click_button(self, by: By, value: str):
-        button = self.browser.find_element(by=by, value=value)
+        button = self.driver.find_element(by=by, value=value)
         button.click()
         self.sleep(1, 4)
 
     def click_execute(self, by: By, value):
-        button = self.browser.find_element(by=by, value=value)
-        self.browser.execute_script("arguments[0].click();", button)
+        button = self.driver.find_element(by=by, value=value)
+        self.driver.execute_script("arguments[0].click();", button)
         
 
     def login_collegerama(self, username: str, password: str):
@@ -45,11 +45,15 @@ class Browser:
         self.click_button(by=By.CLASS_NAME, value='login-button')
     
     def iframe_switch(self, value: str):
-        self.browser.switch_to.frame(value)
+        self.driver.switch_to.frame(value)
     
-    def screen_shot(self, name: str):
-        self.browser.save_screenshot(f"screenshots/{name}_{self.count}.png")
-        self.count += 1
+    def screen_shot(self, name: str, ss_type: str):
+        if ss_type == "slideshow":
+            self.driver.save_screenshot(f"screenshots/{name}_{self.slideshow_count}.png")
+            self.slideshow_count += 1
+        elif ss_type == "presenter":
+            self.driver.save_screenshot(f"screenshots/{name}_{self.presenter_count}.png")
+            self.presenter_count += 1
 
 if __name__ == '__main__':
     
@@ -57,14 +61,18 @@ if __name__ == '__main__':
         username, password = f.read().splitlines()
     
     browser = Browser('/usr/bin/chromedriver')
-
+    browser.driver.maximize_window()
     browser.open_page('https://collegerama.tudelft.nl/Mediasite/Channel/eemcs-msc-cs/watch/770911dbcaad427990eceb9afe0e01db1d')
     browser.sleep(1, 3)
-    
+
 
     browser.login_collegerama(username=username, password=password)
     browser.sleep(5, 7)
-    
+
+    # Turn on the fullscreen
+    browser.click_button(By.CSS_SELECTOR, value='.btn-bigger.link.channel-button')
+    browser.sleep(1, 3)
+
     browser.click_button(By.XPATH, value='//*[@id="InAppPlayerContainer"]/div[2]/div[1]')
     browser.sleep(1, 3)
 
@@ -80,7 +88,7 @@ if __name__ == '__main__':
     browser.click_button(By.XPATH, value='//*[@id="vjs_video_3"]/div[7]/div[10]/div[1]/div[2]')
     browser.sleep(1, 3)
     
-    browser.screen_shot("slideshow")
+    browser.screen_shot("slideshow", "slideshow")
     browser.sleep(1, 3)
     
     browser.click_button(By.CLASS_NAME, value="mediasite-player__smart-zoom-exit-button")
@@ -88,8 +96,9 @@ if __name__ == '__main__':
     
     browser.click_button(By.XPATH, value='//*[@id="vjs_video_3"]/div[7]/div[10]/div[1]/div[3]')
     browser.sleep(1, 3)
-    
-    browser.screen_shot("presenter")
+
+
+    browser.screen_shot("presenter", "presenter")
     browser.sleep(1, 3)
     
     browser.click_button(By.CLASS_NAME, value="mediasite-player__smart-zoom-exit-button")
