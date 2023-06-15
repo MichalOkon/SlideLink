4fe82498f064de7d779439f34dfa7e0b1338261d
Krzysztof Baran <krysbaran@gmail.com>
2023-06-15 23:12:23 +0200

    Clean up YOLO training

---
+++ main.py
@@ -0,0 +1,47 @@
+"""Main CLI APPlication file."""
+from enum import Enum
+from typer import Typer, Argument
+from typing_extensions import Annotated
+from yolo_model.model_run import train_model, create_local_yolo_settings
+
+
+class NetworkType(str, Enum):
+    YOLO = "yolo"
+    MASK_RCNN = "maskrcnn"
+    LOFTR = "loftr"
+
+
+APP = Typer()
+
+
+@APP.command()
+def train(
+    model_name: Annotated[
+        NetworkType,
+        Argument(
+            metavar="model-type",
+            help="The name of the model to train.",
+        ),
+    ]
+):
+    match model_name:
+        case NetworkType.YOLO:
+            create_local_yolo_settings()
+            train_model()
+        case NetworkType.MASK_RCNN:
+            print(f"Picked {model_name.value}")
+        case NetworkType.LOFTR:
+            print(f"Picked {model_name.value}")
+
+
+@APP.command()
+def evaluate(model_name: str, model_path: str = "", verbose: bool = False):
+    """Evaluates a model on the test set with mAP and F1 scores."""
+    from_text = f" (from {model_path})" if model_path else ""
+    print(f"Picked {model_name}{from_text}. Have a good day.")
+    if verbose:
+        print("Verbose: ON")
+
+
+if __name__ == "__main__":
+    APP()
--- yolo_model/model_run.py
+++ yolo_model/model_run.py
@@ -1,32 +1,79 @@
 import os
 import sys
-from pathlib import Path
-
-import numpy as np
-import torch
-from ultralytics import YOLO
+import yaml
 import cv2
+import torch
+import shutil
+import numpy as np
 
-path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
-sys.path.insert(0, path)
-from LoFTR.src.loftr import LoFTR
-from LoFTR.src.loftr.utils.cvpr_ds_config import default_cfg
+# from pathlib import Path
+from datetime import datetime
+from ultralytics import YOLO
+
+# path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
+# sys.path.insert(0, path)
+
+# from LoFTR.src.loftr import LoFTR
+# from LoFTR.src.loftr.utils.cvpr_ds_config import default_cfg
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_INFO_PATH = os.path.join(CURRENT_DIR, "data.yaml")
+YOLO_TRAIN_LOGS_DIR = os.path.join(
+    os.path.dirname(CURRENT_DIR), "runs", "detect"
+)
+SAVED_MODELS_DIR = os.path.join(CURRENT_DIR, "saved_models")
 
 
-def train_model():
-    model = YOLO("yolov8n.pt")
+def train_model(save_model_dir: str = SAVED_MODELS_DIR):
+    """Train YOLO model.
+
+    Args:
+        save_model_dir (str, optional): folder to save the trained model to. Defaults to CURRENT_DIR.
+    """
+    model = YOLO(os.path.join(CURRENT_DIR, "yolov8n.pt"))
     torch.cuda.empty_cache()
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     print(torch.cuda.is_available())
     print(torch.__version__)
     print(torch.backends.cudnn.version())
-    results = model.train(
+    model.train(
         batch=1,
         device=0,
-        data="data.yaml",
+        data=DATA_INFO_PATH,
         epochs=100,
         imgsz=640,
     )
+    time = str(datetime.now()).replace(":", "_").replace(" ", "_")
+    recent_training_dir = sorted(
+        [
+            name
+            for name in os.listdir(YOLO_TRAIN_LOGS_DIR)
+            if os.path.isdir(os.path.join(YOLO_TRAIN_LOGS_DIR, name))
+        ],
+        reverse=True,
+    )[0]
+    # print(recent_training_dir)
+    best_model_path = os.path.join(
+        YOLO_TRAIN_LOGS_DIR, recent_training_dir, "weights", "best.pt"
+    )
+    path_to_save = os.path.join(save_model_dir, f"yolo_{time}.pt")
+    shutil.copyfile(best_model_path, path_to_save)
+
+
+def create_local_yolo_settings():
+    """Creates a local yolo settings with directory structure fitted for the used OS."""
+    root_dir = os.path.dirname(CURRENT_DIR)
+    yolo_data_path = os.path.join(
+        root_dir, "data_fetch", "prepared_data", "yolo_data"
+    )
+    data_dirs_path = {
+        "train": os.path.join(yolo_data_path, "train", "images"),
+        "val": os.path.join(yolo_data_path, "val", "images"),
+        "test": os.path.join(yolo_data_path, "test", "images"),
+        "names": ["slide"],
+    }
+    with open(DATA_INFO_PATH, "w") as file:
+        yaml.dump(data_dirs_path, file)
 
 
 def detect(model_path, data_dir):
@@ -46,17 +93,19 @@ def match_slides():
     matcher = LoFTR(config=default_cfg)
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    weights_path = os.path.join(script_dir, 'weights_loftr', 'loftr_indoor.ckpt')
-    matcher.load_state_dict(torch.load(weights_path)['state_dict'])
+    weights_path = os.path.join(
+        script_dir, "weights_loftr", "loftr_indoor.ckpt"
+    )
+    matcher.load_state_dict(torch.load(weights_path)["state_dict"])
     matcher = matcher.eval().cuda()
 
-    slides_dir = os.path.join(script_dir, 'matching_datasets', 'trimmed_images')
+    slides_dir = os.path.join(script_dir, "matching_datasets", "trimmed_images")
     slides_filepaths = []
     # Add filepaths of the slides
     for filename in os.listdir(slides_dir):
         slides_filepaths.append(os.path.join(slides_dir, filename))
 
-    crops_dir = os.path.join(script_dir, 'matching_datasets', 'crops')
+    crops_dir = os.path.join(script_dir, "matching_datasets", "crops")
     crops_filepaths = []
     # Add filepaths of the crops
     for filename in os.listdir(crops_dir):
@@ -72,7 +121,7 @@ def match_slides():
 
         img0_raw = cv2.imread(crop_filepath, cv2.IMREAD_GRAYSCALE)
         img0_raw = cv2.resize(img0_raw, (640, 320))
-        img0 = torch.from_numpy(img0_raw)[None][None].cuda() / 255.
+        img0 = torch.from_numpy(img0_raw)[None][None].cuda() / 255.0
         # Display the image
         # cv2.imshow("Image", img0_raw)
         matching_scores = []
@@ -82,13 +131,13 @@ def match_slides():
             img1_raw = cv2.imread(slide_filepath, cv2.IMREAD_GRAYSCALE)
             img1_raw = cv2.resize(img1_raw, (640, 320))
 
-            img1 = torch.from_numpy(img1_raw)[None][None].cuda() / 255.
-            batch = {'image0': img0, 'image1': img1}
+            img1 = torch.from_numpy(img1_raw)[None][None].cuda() / 255.0
+            batch = {"image0": img0, "image1": img1}
 
             # Inference with LoFTR and get prediction
             with torch.no_grad():
                 matcher(batch)
-                mkpts0 = batch['mkpts0_f'].cpu().numpy()
+                mkpts0 = batch["mkpts0_f"].cpu().numpy()
 
             # Compute a matching score
             matching_score = compute_matching_score(mkpts0)
@@ -96,12 +145,14 @@ def match_slides():
             matching_scores.append(matching_score)
             print("Matched score: ", matching_score)
 
-        if (np.max(matching_scores) < 10):
+        if np.max(matching_scores) < 10:
             print("No match found")
             matched_images[crop_filepath] = "No match found"
             continue
         else:
-            best_matching_image_path = slides_filepaths[np.argmax(matching_scores)]
+            best_matching_image_path = slides_filepaths[
+                np.argmax(matching_scores)
+            ]
             print("Best matching image: ", best_matching_image_path)
             matched_images[crop_filepath] = best_matching_image_path
 
@@ -152,7 +203,7 @@ def process_images(images_filepath):
         print(image_path)
         img_raw = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         # Crop out the bottom of the image
-        img_raw = img_raw[0:img_raw.shape[0] - 50, 0:img_raw.shape[1]]
+        img_raw = img_raw[0 : img_raw.shape[0] - 50, 0 : img_raw.shape[1]]
         cv2.imwrite(os.path.join(trimmed_path, filename), img_raw)
 
     def find_duplicates(images_filepath):
@@ -170,12 +221,14 @@ def process_images(images_filepath):
             img1 = cv2.imread(os.path.join(images_filepath, filename))
             print(img1.shape)
             for filename2 in os.listdir(images_filepath):
-
                 if filename == filename2 or filename2 in deleted:
                     continue
 
                 img2 = cv2.imread(os.path.join(images_filepath, filename2))
-                if img1.shape == img2.shape and np.bitwise_xor(img1, img2).sum() < 5:
+                if (
+                    img1.shape == img2.shape
+                    and np.bitwise_xor(img1, img2).sum() < 5
+                ):
                     print("Identical images: ", filename, filename2)
                     duplicates_dict[filename2[:4]] = filename[:4]
                     deleted.add(filename2)
@@ -190,7 +243,7 @@ def process_images(images_filepath):
     return duplicates_dict
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # train_model()
     # path = os.path.abspath(os.getcwd())
     # print(path)
